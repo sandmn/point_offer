7ad256d96769ced9008667e9ced408061f035231
sandm <1424193282@qq.com>
2018-06-22 23:25:05 +0800

    测试题

--- every_data_subject/test.c
+++ every_data_subject/test.c
@@ -1,8 +1,34 @@
 #include<stdio.h>
 int main()
 {
-    char str = {"china","beijing"};
-    char *p = str;
-    printf("%s\n",p+10);
+
+    //程序错误，a没有定义
+    int s = 0;
+    {
+        int a = 5;
+        s += a++;
+    }
+    s += a++;
+    printf("%d\n",s);
     return 0;
+
+//    int x,y,z;
+//    scanf("%d%d%d",&x,&y,&z);
+//    printf("%d %d %d\n",x,y,z);
+//    return 0;
+//
+    //char a[5] = {'a','b','\0','c','\0'};
+    //printf("%s",a);
+    //return 0;
+    //unsigned short a = 10;
+    //printf("~a = %lu\n",~a);
+    //char c = 128;
+    //printf("c = %d\n",c);
+    //return 0;
+
+
+    //char str = {"china","beijing"};
+    //char *p = str;
+    //printf("%s\n",p+10);
+    //return 0;
 }
